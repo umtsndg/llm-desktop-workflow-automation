3367a7171e3a59a7171eb324f532bd2263e361f2
Umut Şendağ <umutsendag@hotmail.com.tr>
2026-06-10 17:26:50 +0300

    remove unwanted file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { getDesktopPerception } from './src/desktop/perception';
-
-async function main() {
-    const perception = await getDesktopPerception();
-    console.log(JSON.stringify(perception, null, 2));
-}
-
-main().catch(console.error);
